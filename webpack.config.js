5c1412f3663b659c8ff60f5a6857c3430fa8852a
mitulshah96 <mitulshah96@gmail.com>
2020-04-07 14:01:17 +0530

    added development server config

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,24 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: {
     app: "./src/index.js",
-    print: "./src/print.js"
+    print: "./src/print.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Output Management"
-    })
+      title: "Output Management",
+    }),
   ],
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
@@ -23,16 +28,16 @@ module.exports = {
       { test: /\.(png|svg|jpg|gif)$/, use: ["file-loader"] },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"]
+        use: ["file-loader"],
       },
       {
         test: /\.(csv|tsv)$/,
-        use: ["csv-loader"]
+        use: ["csv-loader"],
       },
       {
         test: /\.xml$/,
-        use: ["xml-loader"]
-      }
-    ]
-  }
+        use: ["xml-loader"],
+      },
+    ],
+  },
 };
